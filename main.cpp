299879431a1b158b483ed863d33519d88b023917
jtroshynski <jtroshynski@gmail.com>
2016-06-27 11:50:56 -0500

    Update main.cpp
    fixed print_list() and add_assignment()

--- main.cpp
+++ main.cpp
@@ -16,15 +16,8 @@
 
 
 using namespace std;
-Assignment project;
 list<Assignment> completed;
 list<Assignment> assigned;
-
-
-
-
-completed.push_front(project);
-
 
 
 //iterate through list and print each assignment
@@ -33,7 +26,7 @@ void print_list(list <Assignment> &li)
 	list<Assignment>::iterator iter = li.begin();
 
 	while (iter != li.end()){
-		cout << *iter.get_due_date() << ", " << *iter.get_description() << ", " << *iter.get_assigned_date() << ", " << *iter.get_status() << endl;
+		cout << (*iter) << endl;
 		++iter;
 	}
 };
@@ -44,24 +37,18 @@ void add_assignment(list <Assignment> &li, Assignment assn)
 {
 	list<Assignment>::iterator iter = li.begin();
 	
-	if (li.is_empty()) //empty list
+	if (li.empty()) //empty list
 	{
-
-	}
+		li.push_front(assn);
+	};
 	
-	while (iter != li.end()) //iterate until correct position
+	while (iter != li.end()) //iterate until the end
 	{
-		if (iter.get_assigned_date() > assn.get_assigned_date()) //if correct position
+		if ((*iter).get_assigned_date() > assn.get_assigned_date()) //if correct position
 		{
 			li.insert(iter, assn);
 			break;
 		}
 		iter++;
 	};
-	Assignment* newassign = new Assignment;
-	newassign = assn;
-	*iter->next->prev = assn; //add to the list
-	current->next = assn;
-	//iterate until current->next assigned_date >= assn assigned_date
-	
 };
